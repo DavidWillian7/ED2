d6980c9f98f178bcd8b02ab94863dd151a87748a
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2022-09-21 19:58:59 -0300

    corrigindo o erro do dataset e criando o metodo de pegar os apps em ingles

--- Analyzer/Analyzer.py
+++ Analyzer/Analyzer.py
@@ -1,4 +1,5 @@
 from csv import reader
+
 class Analyzer:
   def __init__(self,arq):
     self.arq = arq
@@ -21,7 +22,7 @@ class Analyzer:
   def delete_row(self, number_row):
     del self.dataset[number_row]
   
-  def showDuplicates(self):
+  def show_duplicates(self):
     duplicate_apps = []
     unique_apps = []
 
@@ -36,7 +37,7 @@ class Analyzer:
     print('\n')
     print('Examples of duplicate apps:', duplicate_apps[:15])
   
-  def removeDuplicate(self):
+  def remove_duplicate(self):
     reviews_max = {}
 
     for app in self.dataset:
@@ -58,3 +59,24 @@ class Analyzer:
         already_added.append(name)
 
     self.dataset = dataset_clean
+
+  def is_english(self,string):
+    non_ascii = 0
+    for character in string:
+      if ord(character) > 127:
+        non_ascii += 1
+    
+    if non_ascii > 3:
+      return False
+    else:
+      return True
+
+  def clean_english(self, index):
+    dataset_english = []
+
+    for app in self.dataset:
+      name = app[index]
+      if self.is_english(name):
+        dataset_english.append(app)
+    
+    self.dataset = dataset_english
--- Analyzer/main.py
+++ Analyzer/main.py
@@ -10,8 +10,20 @@ dataAndroid = Analyzer('googleplaystore.csv')
 print(dataAndroid.dataset_header)
 dataAndroid.explore_data(3,6,True)
 
-dataAndroid.showDuplicates()
-dataAndroid.removeDuplicate()
+dataAndroid.explore_data(3,6,True)
+dataAndroid.delete_row(10472)
+dataAndroid.explore_data(3,6,True)
+
+dataAndroid.show_duplicates()
+dataAndroid.remove_duplicate()
 
 print(dataAndroid.dataset_header)
 dataAndroid.explore_data(3,6,True)
+
+dataAndroid.clean_english(0)
+dataIos.clean_english(1)
+
+dataAndroid.explore_data(0, 3, True)
+print('\n')
+dataIos.explore_data(0, 3, True)
+
